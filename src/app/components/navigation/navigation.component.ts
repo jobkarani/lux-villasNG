a64cc4bf7324bfedc048c29044435922da45d974
Job Karani <jobkarani084j@gmail.com>
2022-12-30 20:04:02 +0300

    navbar done

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit {
 
+  ngOnInit(): void {
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".nav-menus");
+    const navLink = document.querySelectorAll(".nav-links");
+  
+    const mobileMenu = () => {
+      hamburger?.classList.toggle("active")
+      navMenu?.classList.toggle("active")
+    }
+  
+    const closeMenu = () => {
+      hamburger?.classList.remove("active")
+      navMenu?.classList.remove("active")
+    }
+  
+    hamburger?.addEventListener("click" , mobileMenu)
+    navLink.forEach((l) => l.addEventListener("click",closeMenu))
+  }
+  
 }
