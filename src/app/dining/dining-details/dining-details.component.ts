8413c97e3fcbc8f320b880974e2d992e7207da2e
Job Karani <jobkarani084j@gmail.com>
2023-01-04 01:03:22 +0300

    dining details done

--- src/app/dining/dining-details/dining-details.component.ts
+++ src/app/dining/dining-details/dining-details.component.ts
@@ -27,6 +27,22 @@ export class DiningDetailsComponent implements OnInit {
             console.log(this.singleRestaurant)
           })
       });
+
+      let slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+      let i;
+      let slides = document.getElementsByClassName("mySlides") as HTMLCollectionOf<HTMLElement>;
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+      }
+      slideIndex++;
+      if (slideIndex > slides.length) {slideIndex = 1}    
+      
+      slides[slideIndex-1].style.display = "block";  
+      setTimeout(showSlides, 6000); 
+    }
   }
 
 }
