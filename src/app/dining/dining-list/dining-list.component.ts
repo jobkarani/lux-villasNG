f90140afbab2f5f2cc1f20b16af22c0d7db777ff
Job Karani <jobkarani084j@gmail.com>
2023-01-03 19:32:44 +0300

    consumes the get dinings endpoint

--- src/app/dining/dining-list/dining-list.component.ts
+++ src/app/dining/dining-list/dining-list.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Restaurant } from 'src/app/Interfaces/restaurant';
 
 @Component({
   selector: 'app-dining-list',
   templateUrl: './dining-list.component.html',
   styleUrls: ['./dining-list.component.css']
 })
-export class DiningListComponent {
+export class DiningListComponent implements OnInit{
+
+  constructor(private http:HttpClient) {}
+
+  restaurant: Restaurant[] = [];
+
+  ngOnInit(): void {
+    this.http.get<Restaurant[]>('https://luxury-villasbe.up.railway.app/dining/').subscribe(data =>{
+      this.restaurant = data;
+      console.log(data);
+    })
+  }
 
 }
