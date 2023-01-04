5c9ae4f88c2801d00b86a20a023962d46c4b1efc
Job Karani <jobkarani084j@gmail.com>
2023-01-04 11:25:14 +0300

    consumes get blogs endpoint

--- src/app/blog/blog-list/blog-list.component.ts
+++ src/app/blog/blog-list/blog-list.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Blog } from 'src/app/Interfaces/blog';
 
 @Component({
   selector: 'app-blog-list',
   templateUrl: './blog-list.component.html',
   styleUrls: ['./blog-list.component.css']
 })
-export class BlogListComponent {
+export class BlogListComponent implements OnInit{
 
+  blog:Blog[] = [];
+
+  constructor(private http:HttpClient) {};
+
+  ngOnInit(): void {
+    this.http.get<Blog[]>('https://luxury-villasbe.up.railway.app/blogs/').subscribe(data=>{
+      this.blog = data;
+      console.log(this.blog)
+    })
+      
+  }
 }
