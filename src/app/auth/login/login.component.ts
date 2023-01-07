65101a8cfde7dc2f8b006b97a52484b2a9ba9b61
Job Karani <jobkarani084j@gmail.com>
2023-01-07 17:19:59 +0300

    login page done

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -9,6 +9,18 @@ import { LoginService } from 'src/app/Services/login.service';
 })
 export class LoginComponent implements OnInit{
 
+
+  // container = document.getElementById("container");
+
+  // toggle = () => {
+  //   container.classList.toggle("sign-in");
+  //   container.classList.toggle("sign-up");
+  // };
+
+  // setTimeout(() => {
+  //   container.classList.add("sign-in");
+  // }, 200);
+
   login:any;
 
   constructor(private loginService: LoginService) {}
@@ -26,6 +38,6 @@ export class LoginComponent implements OnInit{
     },
     error => console.log('error',error)
     )
-  }
+  };
 
 }
