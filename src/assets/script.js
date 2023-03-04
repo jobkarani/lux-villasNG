5ac576db7965812b5779ab007c8e07b8eeb26922
Job Karani <jobkarani084j@gmail.com>
2023-03-04 10:29:42 +0300

    fix: solves overflow y issue on both mobile and tab view caused by the navbar

--- src/assets/script.js
+++ src/assets/script.js
@@ -1,20 +1,6 @@
+"use strict";
+
 $(".button-collapse").sideNav();
-
-// if(!/iPhone/i.test(navigator.userAgent)) {
-
-   
-//     $('.active > div > video').get(0).play();
-
-//     $('#carousel').bind('slide.bs.carousel', function(e) {
-//       $(e.relatedTarget).find('video').get(0).play();
-//     });
-
-//     $('#carousel').bind('slid.bs.carousel', function(e) {
-//       $('video').not('.active > div > video').each(function() {
-//         $(this).get(0).pause();
-//       });
-//     });
-//   }
 
   const carousel = $('#carousel');
   setInterval(() => {
